f60061ea4c8d01067220089e7bf1f6ab054b84f9
Mouhamad-gaye <gayemouhamad280@gmail.com>
2025-05-06 14:15:54 -0600

    database connected

--- db/conn.mjs
+++ db/conn.mjs
@@ -1,1 +1,17 @@
-im
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const connectionStr = process.env.mongoURI || ""
+
+async function connectDB() {
+    try {
+        await mongoose.connect(connectionStr)
+        console.log('MongoDB connected...')
+    } catch(err) {
+        console.error(err)
+    }
+}
+
+export default connectDB;
--- server.mjs
+++ server.mjs
@@ -2,15 +2,20 @@ import express from 'express'
 import dotenv from 'dotenv'
 import morgan from 'morgan'
 import cors from 'cors'
-import globalError from 'middleWare'
+import globalError from './middleWare/globalError.mjs'
+import conn from './db/conn.mjs'
+import connectDB from './db/conn.mjs'
 
 const PORT = process.env.PORT || 3001
 const app = express();
 dotenv.config
+connectDB()
+
 
 app.use(express.json());
 app.use(cors());
 app.use(morgan('tiny'))
+
 
 app.use(globalError)
 
